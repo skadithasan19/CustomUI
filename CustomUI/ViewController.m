8b225d58f21e13a2d67a7c19154a7833033ce5d7
Md Adit Hasan <skadithasan19@gmail.com>
2014-03-10 12:24:00 +0600

    From Adit

--- CustomUI/ViewController.m
+++ CustomUI/ViewController.m
@@ -19,6 +19,8 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
        self.navigationItem.leftBarButtonItem=nil;
+    
+    NSLog(@"This from Adit HAsan");
 }
 
 - (void)didReceiveMemoryWarning
